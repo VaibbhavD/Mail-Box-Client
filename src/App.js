4bae3cad1bb072cfdf780956e06043541e49f03a
VaibhavDhamanage <vaibhavdhamanage>
2024-02-25 13:42:31 +0530

    create Routing for Routes path for component reder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import "./App.css";
 import Signup from "./component/Signup/SignUp";
 import Login from "./component/Login/Login";
-import { useState } from "react";
+import { AuthActions } from "./Store/AuthSlice";
+import { useSelector } from "react-redux";
+import { Route, Router, Routes } from "react-router";
+import Layout from "./component/Pages/Layout";
+import Home from "./component/Pages/Home";
+import Mail from "./component/Mail Editer/Mail";
 
 function App() {
-  const [login, setlogin] = useState(true);
-
-  const ChangeAuth = () => {
-    setlogin((prev) => !prev);
-  };
+  const IsLoggedin = useSelector((state) => state.IsLoggedin);
 
   return (
     <>
-      {login && <Login ChangeAuth={ChangeAuth} />};
-      {!login && <Signup ChangeAuth={ChangeAuth} />}
+      <Routes>
+        {!IsLoggedin && <Route path="/" element={<Login />} />}
+        {!IsLoggedin && <Route path="/Signup" element={<Signup />} />}
+        {!IsLoggedin && <Route path="*" element={<Login />} />}
+      </Routes>
+
+      {IsLoggedin && (
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/mailediter" element={<Mail />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </Layout>
+      )}
     </>
   );
 }
