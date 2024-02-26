c1defe84e571939f004e344c0f3733973277e88f
VaibhavDhamanage <vaibhavdhamanage>
2024-02-26 20:25:23 +0530

    add Auth useselecter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Signup from "./component/Signup/SignUp";
 import Login from "./component/Login/Login";
-import { AuthActions } from "./Store/AuthSlice";
 import { useSelector } from "react-redux";
-import { Route, Router, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import Layout from "./component/Pages/Layout";
 import Home from "./component/Pages/Home";
 import Mail from "./component/Mail Editer/Mail";
+import Sent from "./component/Sent/Sent";
+import SentPage from "./component/Pages/SentPage";
+import InboxPage from "./component/Pages/InboxPage";
 
 function App() {
-  const IsLoggedin = useSelector((state) => state.IsLoggedin);
+  const IsLoggedin = useSelector((state) => state.Auth.IsLoggedin);
 
   return (
     <>
@@ -23,7 +25,9 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/inbox" element={<InboxPage />} />
             <Route path="/mailediter" element={<Mail />} />
+            <Route path="/sent" element={<SentPage />} />
             <Route path="*" element={<Home />} />
           </Routes>
         </Layout>
