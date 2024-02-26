257029565919edafe951b6eff93d5e9dabef9818
VaibhavDhamanage <vaibhavdhamanage>
2024-02-26 20:26:24 +0530

    add Style

--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div class="container text-center">
+    <div class="container text-center w-full">
       <h1>Welcome Mail Box</h1>
     </div>
   );
