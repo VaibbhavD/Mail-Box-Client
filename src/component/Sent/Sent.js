696af00092ee7eec71b9d52db175ed0b2e402b01
VaibhavDhamanage <vaibhavdhamanage>
2024-02-26 20:27:26 +0530

    create Sent component

---
+++ src/component/Sent/Sent.js
@@ -0,0 +1,33 @@
+import { current } from "@reduxjs/toolkit";
+import React from "react";
+import { useSelector } from "react-redux";
+import Sentitems from "./Sentitem";
+
+const Sent = () => {
+  const SentEmail = useSelector((state) => state.Email.Sent);
+
+  return (
+    <div class="container ">
+      <div class="row m-auto  ">
+        <div class="col-sm-11 ">
+          <div class="card  ">
+            <div class="card-body bg-primary text-white mailbox-widget pb-0">
+              <h2 class="text-white pb-3">Sent</h2>
+            </div>
+            <div class="table-responsive">
+              <table class="table email-table no-wrap table-hover v-middle mb-0 font-14">
+                <tbody>
+                  {/* <!-- row --> */}
+                  {SentEmail.map((email) => (
+                    <Sentitems email={email} />
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Sent;
