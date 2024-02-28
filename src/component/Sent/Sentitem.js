72f330c6e671e9fc77944d3df8f2d882a46931b1
VaibhavDhamanage <vaibhavdhamanage>
2024-02-28 13:40:07 +0530

    Add mark for unread emails

--- src/component/Sent/Sentitem.js
+++ src/component/Sent/Sentitem.js
@@ -1,24 +1,24 @@
 import React from "react";
+import "../../App.css";
 
 const Sentitems = (porps) => {
   const email = porps.email;
 
   return (
-    <tr class="">
-      {/* <!-- star --> */}
+    <tr className="email">
       <td>
         <i class="fa fa-star text-warning"></i>
       </td>
       <td class="w-25">
-        <span class="mb-0 text-muted">{email.Email}</span>
+        <span class="mb-0 ">{email.Email}</span>
       </td>
       {/* <!-- Message --> */}
-      <td class="w-25">
-        <span class="text-dark ">{email.Subject}</span>
+      <td class="">
+        <span class="text-muted ">{email.Subject}</span>
       </td>
 
       {/* <!-- Time --> */}
-      <td>{email.Time}</td>
+      <td class="text-muted ">{email.Time}</td>
     </tr>
   );
 };
