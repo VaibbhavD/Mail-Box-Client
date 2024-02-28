b1e4e59e4b2e9823577d72ece7899871dd29b157
VaibhavDhamanage <vaibhavdhamanage>
2024-02-28 10:22:05 +0530

    Ad Replace States

--- src/Store/EmailSlice.js
+++ src/Store/EmailSlice.js
@@ -18,6 +18,13 @@ const EmailSlice = createSlice({
     AddTrash: (state, action) => {
       state.Trash.push(action.payload);
     },
+    ReplaceSentEmails: (state, action) => {
+      state.Inbox = action.payload.Inbox;
+      state.Sent = action.payload.Sent;
+      state.Draft = action.payload.Draft;
+      state.Star = action.payload.Star;
+      state.Trash = action.payload.Trash;
+    },
   },
 });
 export const EmailActions = EmailSlice.actions;
