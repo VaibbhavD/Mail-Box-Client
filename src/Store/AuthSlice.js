0bf4281ffd029347d74d96695dd2610110d7f826
VaibhavDhamanage <vaibhavdhamanage>
2024-02-28 10:21:36 +0530

    Add Email State

--- src/Store/AuthSlice.js
+++ src/Store/AuthSlice.js
@@ -1,20 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const Token = localStorage.getItem("toke");
-const InitialAuth = { IsLoggedin: !Token, token: Token };
+const Token = localStorage.getItem("token");
+const LocalEmail = localStorage.getItem("Email");
+const InitialAuth = { IsLoggedin: !!Token, token: null, Email: LocalEmail };
 
 const AuthSlice = createSlice({
   name: "Auth",
   initialState: InitialAuth,
   reducers: {
     Login: (state, action) => {
-      localStorage.setItem("token", action.payload);
+      localStorage.setItem("token", action.payload.idToken);
       state.token = action.payload;
+      let email = action.payload.email;
+      email = email.replaceAll(".", "");
+      email = email.replaceAll("@", "");
+      state.Email = email;
+      localStorage.setItem("Email", email);
       state.IsLoggedin = true;
     },
     Logout: (state, action) => {
       localStorage.removeItem("token");
+      localStorage.removeItem("Email");
       state.token = null;
+      state.Email = null;
       state.IsLoggedin = false;
     },
   },
