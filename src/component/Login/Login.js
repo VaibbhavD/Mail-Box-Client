3e8f19e5aaa7b7777926fd57f80604f296f03b5f
VaibhavDhamanage <vaibhavdhamanage>
2024-02-29 13:18:14 +0530

    Add Navbar component on Login component

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -3,6 +3,7 @@ import { AuthActions } from "../../Store/AuthSlice";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import NavBar from "../SideBar/NavBar";
 
 const Login = () => {
   const EmailRef = useRef();
@@ -39,76 +40,77 @@ const Login = () => {
   };
 
   return (
-    <section class="h-screen">
-      <div class="container pt-5">
-        <div class=" row d-flex justify-content-center align-items-center">
-          <div class="  col-lg-10 col-xl-8 mt-5">
-            <div class="  card text-black">
-              <div class="card-body ">
-                <div class="row justify-content-center ">
-                  <div class="col-md-12 col-lg-6 col-xl-5 order-2 order-lg-1">
-                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
-                      Login
-                    </p>
-
-                    <form onSubmit={SignUpHandler} class="mx-1">
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="email"
-                            id="form3Example3c"
-                            class="form-control"
-                            ref={EmailRef}
-                            required
-                          />
-                          <label class="form-label" htmlFor="form3Example3c">
-                            Your Email
-                          </label>
-                        </div>
-                      </div>
-                      <div class="d-flex flex-row align-items-center mb-4">
-                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div class="form-outline flex-fill mb-0">
-                          <input
-                            type="password"
-                            id="form3Example4c"
-                            class="form-control"
-                            ref={PasswordRef}
-                            required
-                          />
-                          <label class="form-label" htmlFor="form3Example4c">
-                            Password
-                          </label>
-                        </div>
-                      </div>
-                      <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="submit" class="btn btn-primary btn-lg">
-                          Login
-                        </button>
-                      </div>
-                      <p className="text-center">
-                        You Don't Have An Account?{" "}
-                        <Link class="text-black" to={"/signup"}>
-                          Sign Up
-                        </Link>
+    <>
+      <NavBar />
+      <section class="h-screen">
+        <div class="container pt-5">
+          <div class=" row d-flex justify-content-center align-items-center">
+            <div class="  col-lg-10 col-xl-8 mt-5">
+              <div class="  card text-black">
+                <div class="card-body ">
+                  <div class="row justify-content-center ">
+                    <div class="col-md-12 col-lg-6 col-xl-5 order-2 order-lg-1">
+                      <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                        Login
                       </p>
-                    </form>
-                  </div>
-                  <div class="col-md-8 col-lg-6 col-xl-6 d-flex align-items-center order-1 order-lg-2">
-                    <img
-                      src="https://img.freepik.com/free-vector/user-verification-unauthorized-access-prevention-private-account-authentication-cyber-security-people-entering-login-password-safety-measures_335657-3530.jpg?w=740&t=st=1708834373~exp=1708834973~hmac=a37cd460b40a3811b5e7b7ca6e9232e98e3ab9aeda7a22e36bb27ee5a64cd129"
-                      class="img-fluid"
-                      alt="Sample image"
-                    />
+
+                      <form onSubmit={SignUpHandler} class="mx-1">
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <input
+                              type="email"
+                              id="form3Example3c"
+                              class="form-control"
+                              ref={EmailRef}
+                              required
+                            />
+                            <label class="form-label" htmlFor="form3Example3c">
+                              Your Email
+                            </label>
+                          </div>
+                        </div>
+                        <div class="d-flex flex-row align-items-center mb-4">
+                          <div class="form-outline flex-fill mb-0">
+                            <input
+                              type="password"
+                              id="form3Example4c"
+                              class="form-control"
+                              ref={PasswordRef}
+                              required
+                            />
+                            <label class="form-label" htmlFor="form3Example4c">
+                              Password
+                            </label>
+                          </div>
+                        </div>
+                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button type="submit" class="btn btn-primary btn-lg">
+                            Login
+                          </button>
+                        </div>
+                        <p className="text-center">
+                          You Don't Have An Account?{" "}
+                          <Link class="text-black" to={"/signup"}>
+                            Sign Up
+                          </Link>
+                        </p>
+                      </form>
+                    </div>
+                    <div class="col-md-8 col-lg-6 col-xl-6 d-flex align-items-center order-1 order-lg-2">
+                      <img
+                        src="https://img.freepik.com/free-vector/user-verification-unauthorized-access-prevention-private-account-authentication-cyber-security-people-entering-login-password-safety-measures_335657-3530.jpg?w=740&t=st=1708834373~exp=1708834973~hmac=a37cd460b40a3811b5e7b7ca6e9232e98e3ab9aeda7a22e36bb27ee5a64cd129"
+                        class="img-fluid"
+                        alt="Sample image"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 export default Login;
