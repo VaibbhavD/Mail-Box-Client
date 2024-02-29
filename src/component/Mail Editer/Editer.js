3e4d08d0793195569b0dff40a727069e24e7f667
VaibhavDhamanage <vaibhavdhamanage>
2024-02-29 13:17:42 +0530

    Add Modal cart for discard onClick function

--- src/component/Mail Editer/Editer.js
+++ src/component/Mail Editer/Editer.js
@@ -5,11 +5,13 @@ import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import "../../App.css";
 import { useDispatch } from "react-redux";
 import { EmailActions } from "../../Store/EmailSlice";
+import DiscardNotify from "./DiscardNotify";
 
 const Editer = () => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   );
+  const [showNotify, setshowNotify] = useState(false);
   const EmailRef = useRef();
   const SubjectRef = useRef();
   const dispatch = useDispatch();
@@ -35,68 +37,80 @@ const Editer = () => {
     );
   };
 
+  const DiscardHandler = () => {
+    setshowNotify((prev) => !prev);
+    console.log(showNotify);
+  };
+
   return (
-    <div class="container -mt-5">
-      <main class="">
-        <div class="card-body bg-primary text-white mailbox-widget pb-0 rounded col-sm-11  ">
-          <h2 class="text-white pb-4">Compose</h2>
-        </div>
-        <form onSubmit={getTextFromEditor} class=" -mt-5">
-          <div class="form-row  ">
-            <label for="to" class="col-2 col-sm-1 col-form-label">
-              To
-            </label>
-            <div class="col-10 col-sm-11">
-              <input
-                type="email"
-                class="form-control"
-                id="to"
-                placeholder="email"
-                ref={EmailRef}
-              />
-            </div>
+    <>
+      {showNotify && <DiscardNotify setshowNotify={DiscardHandler} />}
+      <div class="container -mt-5">
+        <main class="">
+          <div class="card-body bg-primary text-white mailbox-widget pb-0 rounded col-sm-11  ">
+            <h2 class="text-white pb-4">Compose</h2>
           </div>
-
-          <div class="form-row mb-3 ">
-            <label for="bcc" class="col-2 col-sm-1 col-form-label">
-              Subject
-            </label>
-            <div class="col-10 col-sm-11">
-              <input
-                type="text"
-                class="form-control"
-                id="bcc"
-                placeholder="subject"
-                ref={SubjectRef}
-              />
-            </div>
-          </div>
-
-          <div class="mt-4">
-            <div class="col-sm-11 ml-auto ">
-              <div class=" ">
-                <Editor
-                  editorState={editorState}
-                  onEditorStateChange={handleEditorChange}
-                  toolbarClassName="toolbarClassName"
-                  wrapperClassName="wrapperClassName"
-                  editorClassName="editorClassName"
-                  placeholder="The message goes here..."
+          <form onSubmit={getTextFromEditor} class=" -mt-5">
+            <div class="form-row  ">
+              <label for="to" class="col-2 col-sm-1 col-form-label">
+                To
+              </label>
+              <div class="col-10 col-sm-11">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="to"
+                  placeholder="email"
+                  ref={EmailRef}
                 />
               </div>
             </div>
-          </div>
-          <div class="form-group mt-3">
-            <button type="submit" class="btn btn-success px-3 mx-2">
-              Send
-            </button>
-            <button type="button" class="btn btn-danger mx-2">
-              Discard
-            </button>
-          </div>
-        </form>
-      </main>
-    </div>
+
+            <div class="form-row mb-3 ">
+              <label for="bcc" class="col-2 col-sm-1 col-form-label">
+                Subject
+              </label>
+              <div class="col-10 col-sm-11">
+                <input
+                  type="text"
+                  class="form-control"
+                  id="bcc"
+                  placeholder="subject"
+                  ref={SubjectRef}
+                />
+              </div>
+            </div>
+
+            <div class="mt-4">
+              <div class="col-sm-11 ml-auto ">
+                <div class=" ">
+                  <Editor
+                    editorState={editorState}
+                    onEditorStateChange={handleEditorChange}
+                    toolbarClassName="toolbarClassName"
+                    wrapperClassName="wrapperClassName"
+                    editorClassName="editorClassName"
+                    placeholder="The message goes here..."
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="form-group mt-3">
+              <button type="submit" class="btn btn-success px-3 mx-2">
+                Send
+              </button>
+              <button
+                type="button"
+                class="btn btn-danger mx-2"
+                onClick={DiscardHandler}
+              >
+                Discard
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
   );
 };
 export default Editer;
