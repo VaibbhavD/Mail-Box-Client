13207ad00ad6c4916ef8452546cd8ba5b0c8fd0e
VaibhavDhamanage <vaibhavdhamanage>
2024-02-29 13:16:52 +0530

    Style and customize NavBar

--- src/component/SideBar/NavBar.js
+++ src/component/SideBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
   const dispatch = useDispatch();
 
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           Client MailBox
@@ -23,40 +23,6 @@ const NavBar = () => {
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div class=" collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav ms-auto ">
-            <li class="nav-item">
-              <Link
-                to={"/"}
-                class="nav-link mx-2 active"
-                aria-current="page"
-                href="#"
-              >
-                Home
-              </Link>
-            </li>
-            <li class="nav-item">
-              <Link to={"/mailediter"} class="nav-link mx-2" href="#">
-                Send Mail
-              </Link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link mx-2" href="#">
-                Mail Box
-              </a>
-            </li>
-            <li class="nav-item">
-              <Link
-                to={"/logout"}
-                class="nav-link mx-2"
-                onClick={() => dispatch(AuthActions.Logout())}
-              >
-                LogOut
-              </Link>
-            </li>
-          </ul>
-        </div>
       </div>
     </nav>
   );
