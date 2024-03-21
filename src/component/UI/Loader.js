38e1202ca7473cc472d016c5f2f1a0ac602c5733
VaibhavDhamanage <vaibhavdhamanage>
2024-03-21 18:36:06 +0530

    Add loader component

---
+++ src/component/UI/Loader.js
@@ -0,0 +1,9 @@
+import classes from "./Loader.module.css";
+export const SpinLoader = (
+  <div className={classes.ldsring}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
